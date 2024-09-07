0db4acc944c282cd3970a62e868128004d0f8175
Mariza <marizanfmrocha04@gmail.com>
2024-09-07 18:58:35 -0300

    Update 3

--- 05 - objetos/exercicio-carros.js
+++ 05 - objetos/exercicio-carros.js
@@ -26,18 +26,18 @@ const renault = new Carros('Renault', 'Azul', 90, 20);
 const fiat = new Carros('Fiat', 'Prata', 1, 12)
 const gasolina = 5.57;
 
-function organizar (carro1, carro2, carro3){
+function organizar (carro1){
     if (carro1.marca === 'Chevrolet'){
         const preco = carro1.gasto * gasolina;
         console.log(`A marca do carro é ${carro1.marca}, sua cor é ${carro1.cor} e gasta em média ${carro1.gasto.toFixed(2)}l, sendo o custo da viagem R$${preco.toFixed(2)}`)
     }
-    else if (carro2.marca === 'Renault'){
-        const preco = carro2.gasto * gasolina;
-        console.log(`A marca do carro é ${carro2.marca}, sua cor é ${carro2.cor} e gasta em média ${carro2.gasto.toFixed(2)} litros, sendo o custo da viagem R$${preco.toFixed(2)}`)
+    else if (carro1.marca === 'Renault'){
+        const preco = carro1.gasto * gasolina;
+        console.log(`A marca do carro é ${carro1.marca}, sua cor é ${carro1.cor} e gasta em média ${carro1.gasto.toFixed(2)} litros, sendo o custo da viagem R$${preco.toFixed(2)}`)
     }
-    else if (carro3.marca === 'Fiat'){
-        const preco = carro3.gasto * gasolina;
-        console.log(`A marca do carro é ${carro3.marca}, sua cor é ${carro3.cor} e gasta em média ${carro3.gasto.toFixed(2)} litros, sendo o custo da viagem R$${preco.toFixed(2)}`)
+    else if (carro1.marca === 'Fiat'){
+        const preco = carro1.gasto * gasolina;
+        console.log(`A marca do carro é ${carro1.marca}, sua cor é ${carro1.cor} e gasta em média ${carro1.gasto.toFixed(2)} litros, sendo o custo da viagem R$${preco.toFixed(2)}`)
     }
 }
 organizar(fiat);
